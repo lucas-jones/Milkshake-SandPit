63b4c050a038c843cb5e12a2efbd0b63fdc2615a
Lucas <lucas@Gingerbread.(none)>
2014-10-27 00:29:27 +0000

    Improvements

--- src/SandPit.hx
+++ src/SandPit.hx
@@ -1,6 +1,9 @@
 package ;
 
+import milkshake.core.DisplayObject;
+import milkshake.core.Sprite;
 import milkshake.game.scene.Scene;
+import milkshake.math.Vector2;
 import milkshake.Milkshake;
 
 class SandPit 
@@ -8,21 +11,28 @@ class SandPit
 	static function main() 
 	{
 		var milkshake:Milkshake = Milkshake.boot();
-
-		milkshake.onCreated = function()
-		{
-			milkshake.scenes.addScene(new SampleScene());
-		}
-		
-		trace("Milkshake");
-		trace(milkshake);
+		milkshake.scenes.addScene(new SampleScene());
 	}
 }
 
 class SampleScene extends Scene
 {
+	var gameObject:DisplayObject;
+
 	public function new()
 	{
 		super();
+
+		gameObject = new DisplayObject();
+		var doge = new Sprite("doge.jpg");
+
+		addNode(gameObject);
+		gameObject.addNode(doge);
+
+		//gameObject.x = 100;
+		//gameObject.y = 100;
+		gameObject.scale = Vector2.HALF;
+		doge.anchor = Vector2.HALF;
+		doge.rotation = -1;
 	}
 }
