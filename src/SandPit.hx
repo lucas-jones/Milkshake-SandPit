c36556d85727ce8d97422a1453076854de972467
Lucas A Jones <Necxii@gmail.com>
2014-10-28 21:47:11 +0000

    Making some castles in the SandPit

--- src/SandPit.hx
+++ src/SandPit.hx
@@ -2,6 +2,7 @@ package ;
 
 import milkshake.core.DisplayObject;
 import milkshake.core.Sprite;
+import milkshake.game.scene.camera.Camera;
 import milkshake.game.scene.Scene;
 import milkshake.math.Vector2;
 import milkshake.Milkshake;
@@ -22,7 +23,7 @@ class SampleScene extends Scene
 
 	public function new()
 	{
-		super();
+		super("SampleScene", [ new Camera(100, 100) ]);
 
 		doge = new Sprite("doge.jpg");
 		gameObject = new DisplayObject();
@@ -30,16 +31,18 @@ class SampleScene extends Scene
 		gameObject.addNode(doge);
 		addNode(gameObject);
 
-		gameObject.x = 1280 / 2;
-		gameObject.y = 720 / 2;
-		doge.anchor = Vector2.HALF;
+		// gameObject.x = 1280 / 2;
+		// gameObject.y = 720 / 2;
+		//doge.anchor = Vector2.HALF;
+		doge.width = 1280;
+		doge.height = 720;
 	}
 
 	override public function update(delta:Float):Void
 	{
 		super.update(delta);
 
-		scene.cameras.currentCamera.x++;
-		gameObject.rotation += 0.01;
+		//scene.cameras.currentCamera.x++;
+		//gameObject.rotation += 0.01;
 	}
 }
