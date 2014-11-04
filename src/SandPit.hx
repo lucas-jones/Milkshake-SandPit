eaf2defbd7f4d940a0e3d637417f808f1ef42ae3
Lucas A Jones <Necxii@gmail.com>
2014-11-04 13:53:16 +0000

    Moved PongScene into its own class, started using new collision stuff

--- src/SandPit.hx
+++ src/SandPit.hx
@@ -1,20 +1,7 @@
 package ;
 
-import hxcollision.Collision;
-import hxcollision.shapes.Polygon;
-import hxcollision.shapes.Shape;
-import milkshake.core.DisplayObject;
-import milkshake.core.Graphics;
-import milkshake.core.Sprite;
-import milkshake.game.scene.camera.Camera;
-import milkshake.game.scene.camera.CameraPresets;
-import milkshake.game.scene.Scene;
-import milkshake.math.Vector2;
 import milkshake.Milkshake;
-import milkshake.utils.Color;
-import milkshake.utils.Globals;
-import milkshake.utils.GraphicsHelper;
-import milkshake.utils.MathHelper;
+import scenes.PongScene;
 
 @:expose
 class SandPit
@@ -24,100 +11,6 @@ class SandPit
 	public static function boot()
 	{
 		var milkshake = Milkshake.boot();
-		milkshake.scenes.addScene(new SampleScene());
-	}
-}
-
-class SimpleCollisionObject extends DisplayObject
-{
-	public var shape(default, null):Shape;
-
-	public function new(width:Int, height:Int, color:Int = Color.WHITE)
-	{
-		super();
-
-		addNode(GraphicsHelper.generateRectangle(width, height, color));
-		shape = Polygon.rectangle(0, 0, width, height, false);
-	}
-
-	override public function update(detla:Float):Void
-	{
-		shape.x = x;
-		shape.y = y;
-
-		super.update(detla);
-	}
-}
-
-class SampleScene extends Scene
-{
-	public static inline var PADDLE_WIDTH:Int = 30;
-	public static inline var PADDLE_HEIGHT:Int = 200;
-
-	public static inline var BALL_SIZE:Int = 10;
-	public static inline var BALL_SPEED:Int = 7;
-
-	var paddleLeft:SimpleCollisionObject;
-	var paddleRight:SimpleCollisionObject;
-	var ball:SimpleCollisionObject;
-
-	var ballVelocity:Vector2;
-
-	public function new()
-	{
-		super("SampleScene", CameraPresets.DEFAULT, Color.BLACK);
-
-		addNode(paddleLeft = new SimpleCollisionObject(PADDLE_WIDTH, PADDLE_HEIGHT),
-		{
-			x: 30
-		});
-
-		addNode(paddleRight = new SimpleCollisionObject(PADDLE_WIDTH, PADDLE_HEIGHT),
-		{
-			x: Globals.SCREEN_WIDTH - 30 - paddleRight.width
-		});
-		
-		addNode(ball = new SimpleCollisionObject(BALL_SIZE, BALL_SIZE), 
-		{
-			position: Globals.SCREEN_CENTER
-		});
-
-		resetBall();
-	}
-
-	function resetBall()
-	{
-		ballVelocity = Vector2.EQUAL(BALL_SPEED);
-		ball.position = Globals.SCREEN_CENTER;
-	}
-
-	override public function update(delta:Float):Void
-	{
-		super.update(delta);
-
-		// Ball vs Paddle Collision
-		for(result in Collision.testShapes(ball.shape, [ paddleLeft.shape, paddleRight.shape ]))
-		{
-			ball.x += result.separation.x;
-			ball.y += result.separation.y;
-
-			ballVelocity.multi(Vector2.EQUAL(1.1));
-			ballVelocity.x *= -1;
-		}
-
-		// Paddle Movement
-		paddleLeft.y = MathHelper.clamp(Milkshake.getInstance().mousePosition.y, 0, Globals.SCREEN_HEIGHT - paddleLeft.height);
-		paddleRight.y = MathHelper.clamp(ball.y - paddleLeft.height / 2, 0, Globals.SCREEN_HEIGHT - paddleLeft.height);
-
-		// Ball Velocity
-		ball.position.add(ballVelocity);
-		
-		// Bounce off Top & Bottom
-		if(ball.y < 0 || ball.y + ball.height > Globals.SCREEN_HEIGHT) ballVelocity.y *= -1;
-
-		// Ensure ball is always clamped to screen height
-		ball.y = MathHelper.clamp(ball.y, 0, Globals.SCREEN_HEIGHT - ball.height);		
-
-		if(ball.x > Globals.SCREEN_WIDTH || ball.x < 0) resetBall();
+		milkshake.scenes.addScene(new PongScene());
 	}
 }
---
+++ src/scenes/PongScene.hx
@@ -0,0 +1,98 @@
+package scenes;
+
+import hxcollision.Collision;
+import hxcollision.shapes.Polygon;
+import hxcollision.shapes.Shape;
+import milkshake.core.DisplayObject;
+import milkshake.core.Graphics;
+import milkshake.game.collision.CollisionDisplayObject;
+import milkshake.game.scene.camera.Camera;
+import milkshake.game.scene.camera.CameraPresets;
+import milkshake.game.scene.Scene;
+import milkshake.math.Vector2;
+import milkshake.Milkshake;
+import milkshake.utils.Color;
+import milkshake.utils.Globals;
+import milkshake.utils.GraphicsHelper;
+import milkshake.utils.MathHelper;
+
+class SimpleCollisionObject extends CollisionDisplayObject
+{
+	public function new(width:Int, height:Int, color:Int = Color.WHITE)
+	{
+		super(Polygon.rectangle(0, 0, width, height, false));
+
+		addNode(GraphicsHelper.generateRectangle(width, height, color));
+	}
+}
+
+class PongScene extends Scene
+{
+	public static inline var PADDLE_WIDTH:Int = 30;
+	public static inline var PADDLE_HEIGHT:Int = 200;
+
+	public static inline var BALL_SIZE:Int = 10;
+	public static inline var BALL_SPEED:Int = 7;
+
+	var paddleLeft:SimpleCollisionObject;
+	var paddleRight:SimpleCollisionObject;
+	var ball:SimpleCollisionObject;
+
+	var ballVelocity:Vector2;
+
+	public function new()
+	{
+		super("SampleScene", CameraPresets.DEFAULT, Color.BLACK);
+
+		addNode(paddleLeft = new SimpleCollisionObject(PADDLE_WIDTH, PADDLE_HEIGHT),
+		{
+			x: 30
+		});
+
+		addNode(paddleRight = new SimpleCollisionObject(PADDLE_WIDTH, PADDLE_HEIGHT),
+		{
+			x: Globals.SCREEN_WIDTH - 30 - paddleRight.width
+		});
+
+		addNode(ball = new SimpleCollisionObject(BALL_SIZE, BALL_SIZE));
+
+		resetBall();
+	}
+
+	function resetBall()
+	{
+		ballVelocity = Vector2.EQUAL(BALL_SPEED);
+		ball.position = Globals.SCREEN_CENTER;
+	}
+
+	override public function update(delta:Float):Void
+	{
+		super.update(delta);
+
+		// Ball vs Paddle Collision
+		for(result in Collision.testShapes(ball.shape, [ paddleLeft.shape, paddleRight.shape ]))
+		{
+			ball.x += result.separation.x;
+			ball.y += result.separation.y;
+
+			ballVelocity.multi(Vector2.EQUAL(1.1));
+			ballVelocity.x *= -1;
+		}
+
+		// Paddle Movement
+		//paddleLeft.y = MathHelper.clamp(Milkshake.getInstance().mousePosition.y, 0, Globals.SCREEN_HEIGHT - paddleLeft.height);
+		paddleLeft.y = paddleRight.y = MathHelper.clamp(ball.y - paddleRight.height / 2, 0, Globals.SCREEN_HEIGHT - paddleRight.height);
+
+		// Ball Velocity
+		ball.position.add(ballVelocity);
+
+		// Bounce off Top & Bottom
+		if(ball.y < 0 || ball.y + ball.height > Globals.SCREEN_HEIGHT)
+		{
+			ballVelocity.y *= -1;
+			ball.y = MathHelper.clamp(ball.y, 0, Globals.SCREEN_HEIGHT - ball.height);
+		}
+
+		if(ball.x > Globals.SCREEN_WIDTH || ball.x < 0) resetBall();
+	}
+}
