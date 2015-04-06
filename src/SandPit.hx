6739c1b696369efe1fbfe793592704516a3cad3b
Lucas Jones <lucas.jones@dubitlimited.com>
2015-04-06 14:49:54 +0100

    Switched boilerplate code from index.html into engine

--- src/SandPit.hx
+++ src/SandPit.hx
@@ -2,18 +2,14 @@ package ;
 
 import milkshake.Milkshake;
 import scenes.PlaybackScene;
-import scenes.PolygonScene;
-import scenes.TestScene;
 
 @:expose
 class SandPit
 {
-	static function main() { }
-
-	public static function boot()
+	static function main()
 	{
 		var milkshake = Milkshake.boot();
-		//milkshake.scenes.addScene(new PongScene());
+
 		milkshake.scenes.addScene(new PlaybackScene());
 	}
 }
